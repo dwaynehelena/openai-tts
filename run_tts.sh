d63e35713f316b29c8424ae1b6553699acc97a3c
Dwayne Helena <helenadw@cba.com.au>
2024-10-27 01:48:34 +1100

    Update run_tts.sh

--- run_tts.sh
+++ run_tts.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Activate the virtual environment
+pip3 install -r requirements.txt
+python3 -m venv venv
 source venv/bin/activate
 
 # Run the TTS script
-python tts.py
+python3 tts.py "$1"
+
